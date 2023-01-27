51f04b2d0f0b277b047ae7efd134aec13c158b5f
joseantoniorbieshna <jaramosb05@iesarroyoharnina.es>
2023-01-27 10:28:52 +0100

    Order Files and Folders

--- optionsbbdd.php
+++
@@ -1,6 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$bbdd = "encuestas";
-?>
--- summary.php
+++ summary.php
@@ -4,99 +4,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./main.css" rel="stylesheet"></link>
-    <link href="./style.css" rel="stylesheet"></link>
+    <link href="./Styles/main.css" rel="stylesheet"></link>
+    <link href="./Styles/style.css" rel="stylesheet"></link>
     <script src="main.js"></script>
     <title>Resumen</title>
 
 
     <?php
-        require("config.php");
 
-        error_reporting(0);
-        try{
-            $conexion = new PDO(DB_DSN_CONNECT,DB_USER,DB_PASS);
-            $conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-            $conexion->exec("SET CHARACTER SET utf8");
-            /*Consulta  comentario*/
-            $consulta = "SELECT comentario,fecha FROM encuesta";
-            $resultado = $conexion->query($consulta);
-            $comentario_with_fecha = $resultado->fetchAll(PDO::FETCH_ASSOC);
-            $resultado->closeCursor();
-
-            /*Consulta Puntuacion*/
-            $consulta = "select sum(nota)/count(nota) from encuesta";
-            $resultado = $conexion->query($consulta);
-            $notaAvg = $resultado->fetch();
-            $resultado->closeCursor();
-
-            $conexion = null;
-            
-        }catch (Exception $e){
-            echo "ERROR AL CONECTAR CON LA BASE DE DATOS";
+        session_start();
+        if(!isset($_SESSION["Usuario"])){
+            header("Location:login.php");
         }
 
-    /*FUNCIONES*/
+        require("./Class/DatosSummary.php");
 
-        /*IMPRIMIR COMENTARIO IZQ Y DERECHA*/
-        function imprimirComentario($lado){
-            global $comentario_with_fecha;
-            $guardado="";
-            if($comentario_with_fecha!==NULL && ( $lado=="izquierda" || $lado=="derecha" || $lado=="todos" ) ){
-                switch ($lado){
-                    case "izquierda":
-                        for($i=0;$i<count($comentario_with_fecha);$i++){
-                            if($i==0 || $i%2==0){
-                                $guardado.="
-                                <div class='comment box'>
-                                <p id='comment-text'>".$comentario_with_fecha[$i]["comentario"]."</p>
-                                <p id='date'>".$comentario_with_fecha[$i]["fecha"]."</p>
-                                </div>";
-                            }
-                        }
-                        return $guardado;
-                        break;
-                    case "derecha":
-                        for($i=0;$i<count($comentario_with_fecha);$i++){
-                            if($i%2!=0){
-                                $guardado.="
-                                <div class='comment box'>
-                                <p id='comment-text'>".$comentario_with_fecha[$i]["comentario"]."</p>
-                                <p id='date'>".$comentario_with_fecha[$i]["fecha"]."</p>
-                                </div>";
-                            }
-                        }
-                        return $guardado;
-                        break;
-                    case "todos":
-                        for($i=0;$i<count($comentario_with_fecha);$i++){
-                           
-                            $guardado.="
-                            <div class='comment box'>
-                            <p id='comment-text'>".$comentario_with_fecha[$i]["comentario"]."</p>
-                            <p id='date'>".$comentario_with_fecha[$i]["fecha"]."</p>
-                            </div>";
-                            
-                        }
-                        return $guardado;
-                        break;
-                }
-            }else{
-                return "<div class='comment box'>
-                <p id='comment-text'>ERROR</p>
-                <p id='date'>ERROR</p>
-                </div>";
-            }
+        $conexion = new DatosSummary();
+        if($conexion->db_conexion!=NULL){
+            $conexion->setDatosComentario();
+            $conexion->setDatosGenerales();
         }
 
-        /*IMPRIMIR MENSAJE DEPENDE DE SI ES NULL O NO*/
-        function switchPrintValueOrNullMessage($valorCompNull,$no_conecta,$conecta){
-            if($valorCompNull===NULL){
-                echo $no_conecta;
-            }else{
-                echo $conecta;
-            }
-        }
+
     ?>
 
 </head>
@@ -110,19 +39,21 @@
                 <span></span>
                 <span></span>
                 <ul id="menu">
-                  <a href="./index.html"><li>Inicio</li></a>
-                  <a href="./form.html"><li>Encuesta</li></a>
+                  <a href="./index"><li>Inicio</li></a>
+                  <a href="./form"><li>Encuesta</li></a>
                   <a href="#"><li>Resumen</li></a>
                   <a href="#"><li>Contacto</li></a>
+                  <a href="./cerrar_sesion.php"><li>Cerrar Sesion</li></a>
                 </ul>
             </div>
         </div>
         
         <div class="menu">
-            <img src="./logo-example.png" alt="logo">
+            <img src="./Resources/logo-example.png" alt="logo">
             <a href="./index.html">Inicio</a>
             <a href="./form.html">Encuesta</a>
             <a href="">Contacto</a>
+            <a href="./cerrar_sesion.php">Cerrar Sesion</a>
         </div>
 
 
@@ -137,7 +68,7 @@
                             <tbody>
                                 <tr>
                                     <td>Valoración</td>
-                                    <td> <?php switchPrintValueOrNullMessage($notaAvg,"-",round($notaAvg[0],1)); ?> </td>
+                                    <td> <?php DatosSummary::switchPrintNullOrValueMessage($conexion->getDatosGenerales(),"-",$conexion->getDatosGenerales()); ?> </td>
                                 </tr>
                                 <tr>
                                     <td>Satisfecho</td>
@@ -172,14 +103,16 @@
             </div>
             <div class="comments">
                 <div class="comments-left">
-                    <?php switchPrintValueOrNullMessage($comentario_with_fecha,imprimirComentario("error"),imprimirComentario("izquierda")); ?>
+                    <?php DatosSummary::switchPrintNullOrValueMessage($conexion->getDatosComentario(),$conexion->printCommentAndDate("error"),$conexion->printCommentAndDate("izquierda")); ?>
                 </div>
                 <div class="comments-right">
-                    <?php switchPrintValueOrNullMessage($comentario_with_fecha,imprimirComentario("error"),imprimirComentario("derecha")); ?>
+                    <?php DatosSummary::switchPrintNullOrValueMessage($conexion->getDatosComentario(),$conexion->printCommentAndDate("error"),$conexion->printCommentAndDate("derecha")); ?>
                 </div>
                 <div class="comments-media">
-                    <?php switchPrintValueOrNullMessage($comentario_with_fecha,imprimirComentario("error"),imprimirComentario("todos")); ?>
+                    <?php DatosSummary::switchPrintNullOrValueMessage($conexion->getDatosComentario(),$conexion->printCommentAndDate("error"),$conexion->printCommentAndDate("todos")); ?>
                 </div>
+
+                
             </div>
         </div>
         <div class="footer">
