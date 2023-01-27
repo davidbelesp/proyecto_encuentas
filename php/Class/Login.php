836abe4fb46c9e1fc2866abc184f9fc74ee828a4
joseantoniorbieshna <117735985+joseantoniorbieshna@users.noreply.github.com>
2023-01-27 01:02:55 +0100

    Create class Login

---
+++ php/Class/Login.php
@@ -0,0 +1,46 @@
+<?php
+require("Conexion.php");
+class login extends Conexion{
+    private $user;
+    private $pass;
+    public function __construct($user_post,$pass_post){
+        parent::__construct();
+        $this->user = htmlentities(addslashes($user_post));
+        $this->pass = htmlentities(addslashes($pass_post));
+    }
+    public function iniciarSesion(){
+        try{
+
+            $consulta = "SELECT Usuario from usuarios where usuario = :user and password = :pass";
+            $resultado = $this->db_conexion->prepare($consulta);
+    
+    
+            $resultado->bindValue(":user",$this->user);
+            $resultado->bindValue(":pass",$this->pass);
+            $resultado->execute();
+            $numero_resultado_coincidencia = $resultado->rowCount();
+            if($numero_resultado_coincidencia!=0){
+                session_start();
+                $_SESSION["Usuario"] = $this->user;
+    
+                header("location:summary.php");
+            }else{
+                header("location:index.html");
+            }
+
+        }catch(Exception $excepcion){
+
+            echo "ERROR AL REALIZAR LA CONSULTA:";
+            echo "<br><br>";
+            echo "ERROR:". $excepcion->getMessage();
+        }
+
+    }
+    public function cerrarSession(){
+        session_start();
+        session_destroy();
+        header("Location:index.html");
+    }
+
+}
+?>
--- php/login.php
+++ php/login.php
@@ -1,31 +1,5 @@
     <?php
-        try{
-            $conexion = new PDO("mysql:host=localhost;dbname=encuestas","root","");
-            $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $consulta = "SELECT Usuario from usuarios where usuario = :user and password = :pass";
-            $resultado = $conexion->prepare($consulta);
-
-            $login = htmlentities(addslashes($_POST["user_login"]));
-            $password = htmlentities(addslashes($_POST["password_login"]));
-
-            $resultado->bindValue(":user",$login);
-            $resultado->bindValue(":pass",$password);
-            $resultado->execute();
-            
-            $numero_resultado_coincidencia = $resultado->rowCount();
-            if($numero_resultado_coincidencia!=0){
-                session_start();
-
-                $_SESSION["Usuario"] = $_POST["user_login"];
-
-                header("location:summary.php");
-            }else{
-                header("location:index.html");
-            }
-
-
-        }catch(Exception $e){
-            echo "ERROR: ". $e->getMessage();
-
-        }
+        require("./Class/Login.php");
+        $login = new Login($_POST["user_login"],$_POST["password_login"]);
+        $login->iniciarSesion();
     ?>
