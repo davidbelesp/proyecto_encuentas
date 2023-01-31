250c505bea7c0beedc1b5ba1c1270d1f89a1741f
David <dbeltrane01@iesarroyoharnina.es>
2023-01-31 22:38:09 +0100

    Major Format Update
    + Improved Format on most documents
    - Deleted unused and testing files

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,12 +1,12 @@
 var data;
 
-function redirect(url){
+function redirect(url) {
     location.href = url;
 }
 
-function noWLogo(){
+function noWLogo() {
     const divs = document.getElementsByTagName("div");
-    const div = divs[divs.length-1];
+    const div = divs[divs.length - 1];
     try {
         if (div.firstChild == null) return;
         if (div.firstChild.getElementsByTagName("a") && div.firstChild.title == "Hosted on free web hosting 000webhost.com. Host your own website for FREE.") div.remove();
@@ -16,7 +16,7 @@ function noWLogo(){
     }
 }
 
-function changeIndexPage(event = null){
+function changeIndexPage(event = null) {
     const classes = event.target.className;
     const wrapper = document.querySelector(".wrapper");
     const downButton = document.querySelector(".down");
@@ -25,24 +25,24 @@ function changeIndexPage(event = null){
     const nav = document.querySelector(".nav-buttons");
 
     currentPage = wrapper.classList[1];
-    currentPage = parseInt(currentPage[currentPage.length-1]);
+    currentPage = parseInt(currentPage[currentPage.length - 1]);
 
-    if (currentPage <= 1 && classes.includes("up") 
-    || currentPage >= 3 && classes.includes("down")) return;
+    if (currentPage <= 1 && classes.includes("up")
+        || currentPage >= 3 && classes.includes("down")) return;
 
-    if (classes.includes("up")) landPage = currentPage-1;
-    else if (classes.includes("down")) landPage = currentPage+1;
+    if (classes.includes("up")) landPage = currentPage - 1;
+    else if (classes.includes("down")) landPage = currentPage + 1;
 
     nav.classList.add("invisible");
     wrapper.classList.remove(`active-page${currentPage}`);
 
-    if (classes.includes("up")){
+    if (classes.includes("up")) {
         if (landPage == 1) upButton.classList.add("invisible");
-        setTimeout(() => {downButton.classList.remove("invisible")}, 400);
+        setTimeout(() => { downButton.classList.remove("invisible") }, 400);
         wrapper.classList.add(`active-page${landPage}`)
-    } else if (classes.includes("down")){
+    } else if (classes.includes("down")) {
         if (landPage == 3) downButton.classList.add("invisible")
-        setTimeout(() => {upButton.classList.remove("invisible")}, 400);
+        setTimeout(() => { upButton.classList.remove("invisible") }, 400);
         wrapper.classList.add(`active-page${landPage}`);
     } else return console.log("Evento no valido");
 
@@ -51,21 +51,21 @@ function changeIndexPage(event = null){
     }, 800);
 }
 
-async function importJson(){
+async function importJson() {
     const jsonPath = "./Resources/badWords.json";
     const response = await fetch(jsonPath);
     data = await response.json();
 }
 
-function validateForm(event){
+function validateForm(event) {
 
-    const words = [...data["EN"]["words"],...data["ES"]["words"]];
+    const words = [...data["EN"]["words"], ...data["ES"]["words"]];
 
     let text = "";
     text = document.getElementById("comentario").value.split(' ');
-    
+
     text.forEach(element => {
-        if (words.includes(element)){
+        if (words.includes(element)) {
             event.preventDefault()
             alert("validation failed false"); //PROCEDURE TO NOT VALIDATE
             return false;
--- form.php
+++ form.php
@@ -1,24 +1,27 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-    <link href="./Styles/style.css" rel="stylesheet"></link>
-    <link href="./Styles/main.css" rel="stylesheet" >
-    <link href="./Styles/form.css" rel="stylesheet" >
+    <link href="./Styles/style.css" rel="stylesheet">
+    </link>
+    <link href="./Styles/main.css" rel="stylesheet">
+    <link href="./Styles/form.css" rel="stylesheet">
     <script src="./Scripts/main.js"></script>
     <title>formulario</title>
 </head>
+
 <body>
     <?php
     require("./Class/Form.php");
     session_start();
     $conexion = new Form();
-    if(isset($_POST["enviar"])){
+    if (isset($_POST["enviar"])) {
         $conexion->setProfesor($_POST["profesor"]);
-        $conexion->setComment($_POST["comentario"],$_POST["nota"]);
+        $conexion->setComment($_POST["comentario"], $_POST["nota"]);
         $conexion->enviarFormulario();
         header("Location: index");
     }
@@ -26,18 +29,24 @@
     <div class="page">
         <div class="hamburger-menu">
             <div id="menuToggle" class="no-selectable">
-                <input type="checkbox"/>
+                <input type="checkbox" />
                 <span></span>
                 <span></span>
                 <span></span>
                 <ul id="menu">
-                  <a href="./index"><li>Inicio</li></a>
-                  <a href="./summary"><li>Resumen</li></a>
-                  <a href="#"><li>Contacto</li></a>
+                    <a href="./index">
+                        <li>Inicio</li>
+                    </a>
+                    <a href="./summary">
+                        <li>Resumen</li>
+                    </a>
+                    <a href="#">
+                        <li>Contacto</li>
+                    </a>
                 </ul>
             </div>
         </div>
-        
+
         <div class="menu">
             <img src="./Resources/logo-example.png" alt="logo">
             <a href="./index">Inicio</a>
@@ -46,28 +55,58 @@
         </div>
 
         <div class="content">
-            <div class="form-box"> 
-                <form id="myForm" action="<?php echo $_SERVER ["PHP_SELF"];?>" method="post" id="form">
+            <div class="form-box">
+                <form id="myForm" action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post" id="form">
                     <select name="profesor" id="selectProfesor">
                         <?php $conexion->PrintProfesoresHTMLSelect(); ?>
                     </select>
 
                     <p>Nota</p>
                     <div class="nota fancybox">
-                        <input id="notaInput" type="range" name="nota" value="1" min="1" max="10" oninput="updateNotaNumber(event)" required>    
+                        <input id="notaInput" type="range" name="nota" value="1" min="1" max="10"
+                            oninput="updateNumber(event,'nota')" required>
 
-                        <p id="number">1</p>
+                        <p id="numberNota">1</p>
                     </div>
 
+                    <span id="form-div"></span> <!--------------------------------------------------------->
 
+                    <p>Satisfaccion</p>
+                    <div class="election">
+                        <input type="radio" name="satisf" id="si">
+                        <input type="radio" name="satisf" id="no">
+                    </div>
 
-                    <span id="form-div"></span>
+                    <span id="form-div"></span> <!--------------------------------------------------------->
+
+                    <p>Examenes</p>
+
+                    <div class="examenes fancybox">
+                        <input id="examInput" type="range" name="exam" value="1" min="1" max="10"
+                            oninput="updateNumber(event,'exam')" required>
+
+                        <p id="numberExam">1</p>
+                    </div>
+
+                    <span id="form-div"></span> <!--------------------------------------------------------->
+
+                    <p>Tareas</p>
+
+                    <div class="tareas fancybox">
+                        <input id="tareaInput" type="range" name="tareas" value="1" min="1" max="10"
+                            oninput="updateNumber(event,'tarea')" required>
+
+                        <p id="numberTarea">1</p>
+                    </div>
+
+                    <span id="form-div"></span> <!--------------------------------------------------------->
 
                     <p>Comentario</p>
-                    <textarea type="text" id="comentario" name="comentario" maxlength="200" placeholder="Escribe un comentario para el profesor"></textarea>
+                    <textarea type="text" id="comentario" name="comentario" maxlength="200"
+                        placeholder="Escribe un comentario para el profesor"></textarea>
 
-                    <span id="form-div"></span>
-                    
+                    <span id="form-div"></span> <!--------------------------------------------------------->
+
                     <button type="submit" onclick="return validateForm(event)" name="enviar" id="send">Enviar</button>
                 </form>
             </div>
@@ -79,13 +118,30 @@
 
 </body>
 <script>
-    function updateNotaNumber(event){
-        let numberHTML = document.getElementById("number")
-        numberHTML.innerHTML = event.target.value
+    function updateNumber(event, mode) {
+        let numberNota = document.getElementById("numberNota");
+        let numberExam = document.getElementById("numberExam");
+        let numberTarea = document.getElementById("numberTarea");
+
+        switch (mode) {
+            case "nota":
+                numberNota.innerHTML = event.target.value;
+                break;
+            case "exam":
+                numberExam.innerHTML = event.target.value;
+                break;
+            case "tarea":
+                numberTarea.innerHTML = event.target.value;
+                break;
+            default:
+                alert("EXCEPTION CHANGING NUMBER")
+                break;
+        }
     }
 
 </script>
 <script>
-setTimeout(() => {noWLogo()}, 0);
+    setTimeout(() => { noWLogo() }, 0);
 </script>
+
 </html>
